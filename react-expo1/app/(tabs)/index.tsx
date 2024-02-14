cee1175af54ee8dca31ca0be7999bb990de9bcff
naziya0506 <your.email@example.com>
2024-02-14 15:49:52 +0530

    try-expo

--- react-expo1/app/(tabs)/index.tsx
+++ react-expo1/app/(tabs)/index.tsx
@@ -1,31 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button, Alert, Dimensions } from 'react-native';
 
 
 
 export default function App() {
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text>HELLO REACT WORLD. </Text>
+      <Text><h1>HELLO REACT WORLD.</h1> </Text>
       <TouchableOpacity onPress={ () => console.log("image clicked")}>
       <Image
        source={{ 
-        width: 200,
-        height: 300,
+        width: 500,
+        height: 400,
         uri: "https://picsum.photos/200/300"}} />
         </TouchableOpacity>
-        <Button title='Click me' onPress={ () => console.log("clicked me")}/>
+
+        <Button 
+        color="green"
+        title='Click me' 
+        onPress={ () => alert("button tapped")}/>
+
+        {/* <View
+        style={{
+          backgroundColor: "blue",
+          width: "50%",
+          height: 80
+        }}
+        ></View> */}
     </SafeAreaView>
+
+        // <View
+        //   style={{
+        //     backgroundColor: "teal",
+        //     width: 200,
+        //     height: 100,
+        //     flex: 0.5,
+        //     flexDirection: "row",
+        //     justifyContent: "center",
+        //   }}>
+
+        //   <View
+        //   style={{
+        //     backgroundColor: "black",
+        //     width: 200,
+        //     height: 100,
+        //     flex: 0.5,
+        //     flexDirection: "row",
+        //     justifyContent: "center",
+        //   }}/>
+
+        //   <View
+        //   style={{
+        //     backgroundColor: "gold",
+        //     width: 200,
+        //     height: 100,
+        //     flex: 0.5,
+        //     flexDirection: "row",
+        //     justifyContent: "space-around",
+        //   }}/>
+        // </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: 'lightash',
-    alignItems: 'center',
-    justifyContent: 'center'
+    // alignItems: 'center',
+    // justifyContent: 'center'
   
   },
   title: {
@@ -33,8 +76,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    // marginVertical: 30,
+    // height: 1,
+    // width: '80%',
   },
 });
